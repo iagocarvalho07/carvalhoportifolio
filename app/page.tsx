394f016ba395526715243debb4e6083695345ef8
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2025-01-28 13:23:43 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Approach from "@/components/Approach";
 import Grind from "@/components/Grind";
 import Hero from "@/components/Hero";
@@ -13,7 +11,14 @@ import { FaDiagramProject, FaEnvelope, FaPersonChalkboard } from "react-icons/fa
 export default function page() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full overflow-auto">
+
+    </main>
+
+  );
+}
+
+/**
+ *       <div className="max-w-7xl w-full overflow-auto">
         <FloatingNav navItems={[
           { name: 'home', link: "#", icon: <FaHome /> },
           { name: 'Sobre', link: "#about", icon: <FaPersonChalkboard /> },
@@ -27,7 +32,4 @@ export default function page() {
         <Approach />
       </div>
       <Myfooter />
-    </main>
-
-  );
-}
+ */
