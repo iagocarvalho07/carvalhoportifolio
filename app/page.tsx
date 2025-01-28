21e912d158b6ea66c016eebb6d0e70998e6308cd
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2025-01-28 12:40:48 -0300

    try

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,25 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import WorkExperience from "@/components/WorkExperience";
 import { FaHome } from "react-icons/fa";
-import { FaDiagramProject, FaEnvelope, FaPersonChalkboard} from "react-icons/fa6";
+import { FaDiagramProject, FaEnvelope, FaPersonChalkboard } from "react-icons/fa6";
 
-export default function Home() {
+export default function page() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full overflow-auto">
         <FloatingNav navItems={[
           { name: 'home', link: "#", icon: <FaHome /> },
-          { name: 'Sobre', link: "#about", icon: <FaPersonChalkboard  /> },
-          { name: 'Projetos', link: "#projects", icon: <FaDiagramProject  /> },
-          { name: 'Experiencia', link: "#experience", icon: <FaEnvelope  /> },
+          { name: 'Sobre', link: "#about", icon: <FaPersonChalkboard /> },
+          { name: 'Projetos', link: "#projects", icon: <FaDiagramProject /> },
+          { name: 'Experiencia', link: "#experience", icon: <FaEnvelope /> },
         ]} />
         <Hero />
         <Grind />
-       <RecentProjects/>
-        <WorkExperience/>
-        <Approach/>
+        <RecentProjects />
+        <WorkExperience />
+        <Approach />
       </div>
-      <Myfooter/>
+      <Myfooter />
     </main>
 
   );
