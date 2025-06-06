43a40ffbc7c18f8a0609715050c9a25d973e45ed
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2025-06-05 21:07:09 -0300

    teste vercel 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import Approach from "@/components/Approach";
 import Grind from "@/components/Grind";
-import Hero from "@/components/Hero";
 import Myfooter from "@/components/Myfooter";
 import RecentProjects from "@/components/RecentProjects";
 import WorkExperience from "@/components/WorkExperience";
 import { FaHome } from "react-icons/fa";
 import { FaDiagramProject, FaEnvelope, FaPersonChalkboard } from "react-icons/fa6";
-import { Suspense } from 'react'
-import { FloatingNav } from "@/components/ui/FloatingNav";
+import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
 const DynamicHero = dynamic(() => import('@/components/Hero'), {
+  ssr: false,
+  loading: () => <div>Carregando...</div>
+});
+
+const DynamicFloatingNav = dynamic(() => import('@/components/ui/FloatingNav').then(mod => mod.FloatingNav), {
   ssr: false,
   loading: () => <div>Carregando...</div>
 });
@@ -20,7 +23,7 @@ export default function Page() {
     <Suspense>
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full overflow-auto">
-          <FloatingNav navItems={[
+          <DynamicFloatingNav navItems={[
             { name: 'home', link: "#", icon: <FaHome /> },
             { name: 'Sobre', link: "#about", icon: <FaPersonChalkboard /> },
             { name: 'Projetos', link: "#projects", icon: <FaDiagramProject /> },
